9faf553537735d23fbb0dff2addcdcfba9eaeeb7
ruenocos <wessel.oud@Outlook.com>
2021-06-28 01:11:22 +0000

    Deploying to gh-pages from @ ruenocos/CapsFromThePast@34a65e611ecae7bef23c08bf04ab328e2e15858b 🚀

---
+++ _nuxt/static/1624842681/products/ExampleProduct2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCapsFromThePast\u002F_nuxt\u002Fstatic\u002F1624842681",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproducts\u002FExampleProduct2",config:{_app:{basePath:"\u002FCapsFromThePast\u002F",assetsPath:"\u002FCapsFromThePast\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"462c9fc7"}}}}(null));
